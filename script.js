4cece7955f8900ed3e17ad0784b7aadf2ea66737
KrishnanProvility <krishnan@provility.com>
2024-12-30 18:42:16 +0530

    Completed word game

--- script.js
+++ script.js
@@ -51,62 +51,86 @@ const WORDS = [
   "SOUND",
 ];
 let currentRow = 0;
-let randomWord='';
+let randomWord = "";
 function startGame() {
-    randomWord = randomWordGen();
-    console.log(randomWord);
-    clearTable();
+  randomWord = randomWordGen();
+  console.log(randomWord);
+  clearTable();
 }
 function wordGame() {
-const inputWord = document.querySelector(".inputValue").value.toUpperCase();
-
-checkWord(randomWord,inputWord)
-
-  
+  const inputWord = document.querySelector(".inputValue").value.toUpperCase();
+  if (inputWord != "" && randomWord != "") {
+    checkWord(randomWord, inputWord);
+  } else {
+    alert("Please press start button and give input to start ");
+  }
 }
 
 function randomWordGen() {
   const charachters = WORDS;
-    const randomNumber = Math.round(Math.random() * charachters.length);
-    return randomWord =charachters[randomNumber] ;
+  const randomNumber = Math.round(Math.random() * charachters.length);
+  return (randomWord = charachters[randomNumber]);
 }
 
-
-function checkWord(randomword,inputWord){
-if(randomword==inputWord){
-    clearTable();
-    return alert(`Word is ${randomword}.\nYou answered correctly. `)
-}
-else
-{
-addWord(inputWord);
-}
-}
-function clearTable() {
-    const rows = document.querySelectorAll('.table tr');
-    rows.forEach(row => {
-        row.querySelectorAll('td').forEach(cell => {
-            cell.textContent = ''; 
-        });
-    });
-    currentRow = 0;
-}
-
-function addWord(inputWord){
-    const characters = inputWord.split("");
-    const rows = document.querySelectorAll(".table tr");
-    if (currentRow < rows.length) {
-        const currentTableRow = rows[currentRow];
-        characters.forEach((char, index) => {
-          if (index < 5) {
-            currentTableRow.querySelectorAll("td")[index].textContent = char;
-          }
-        });
-        currentRow++;
-      }
-      if(currentRow>=6)
-        alert(`Maximum number of rows reached!\nWord is ${randomWord}.\nYou answered correctly. `);
+function checkWord(randomword, inputWord) {
+  if (randomword == inputWord) {
+    addWord(randomword, inputWord);
     
-    const clearinput = document.querySelector(".inputValue");
-    clearinput.value = '';
+     
+     return 
+  }
+  addWord(randomword, inputWord);
+  setTimeout(() => {
+    maxReach(currentRow, randomword);
+    
+  }, 4000);
+  
+}
+
+function addWord(randomword, inputWord) {
+  const characters = inputWord.split("");
+  const rows = document.querySelectorAll(".table tr");
+  if (currentRow <= rows.length) {
+    const currentTableRow = rows[currentRow];
+    const color = matchColor(randomword, inputWord);
+    characters.forEach((char, index) => {
+      if (index < 5) {
+        currentTableRow.querySelectorAll("td")[index].textContent = char;
+        currentTableRow.querySelectorAll("td")[index].bgColor = color[index];
+      }
+    });
+    currentRow++;
+  }
+  const clearinput = document.querySelector(".inputValue");
+  clearinput.value = "";
+}
+
+function matchColor(randomWord, inputWord) {
+  let colors = [];
+  for (let char of inputWord) {
+    const index = randomWord.indexOf(char);
+    if (index !== -1) {
+      colors.push(randomWord[index] === inputWord[index] ? "green" : "yellow");
+    } else {
+      colors.push("grey");
+    }
+  }
+  return colors;
+}
+
+function clearTable() {
+  const rows = document.querySelectorAll(".table tr");
+  rows.forEach((row) => {
+    row.querySelectorAll("td").forEach((cell) => {
+      cell.textContent = "";
+      cell.bgColor = "";
+    });
+  });
+  currentRow = 0;
+}
+function maxReach(currentRow, randomword) {
+  if (currentRow >= 5) {
+    alert(`max table reached\nWord is ${randomword}.`);
+    clearTable();
+  }
 }
